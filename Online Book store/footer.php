8f1b40134fa7de55700f891d518404d45a7ac9fe
Shubham Awasthi_07 <90404728+ShubhamAwasthi07@users.noreply.github.com>
2022-10-04 10:06:50 +0530

    Add files via upload

---
+++ Online Book store/footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>Quick Links</h3>
+         <a href="home.php">Home</a>
+         <a href="about.php">About</a>
+         <a href="shop.php">Shop</a>
+         <a href="contact.php">Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra Links</h3>
+         <a href="login.php">Login</a>
+         <a href="register.php">Register</a>
+         <a href="cart.php">Cart</a>
+         <a href="orders.php">Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Info</h3>
+         <p> <i class="fas fa-phone"></i> +123-456-7890 </p>
+         <p> <i class="fas fa-phone"></i> +111-222-3333 </p>
+         <p> <i class="fas fa-envelope"></i>ShubhamAwasthi@gmail.com</p>
+         <p> <i class="fas fa-map-marker-alt"></i> Lucknow, india - 400104 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="https://twitter.com/Shubham76444761"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="https://www.linkedin.com/in/shubham-awasthi-3425871b5/"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>Shubham Awasthi</span> </p>
+
+</section>
